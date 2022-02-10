6411920dbc1d6a1180fa1b527678a839b788aa6f
YAMA-Te <99267757+YAMA-Te@users.noreply.github.com>
2022-02-10 22:42:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import streamlit as st
-#import requests
-#import json
-#import datetime
-#import pandas as pd
-#import re
+import requests
+import json
+import datetime
+import pandas as pd
+import re
 
 st.title("大分県関係の番組")
 
-#z=("大分")
+z=("大分")
 
-#t_delta = datetime.timedelta(hours=9)  
-#JST = datetime.timezone(t_delta, 'JST')  
-#dt = datetime.datetime.now(JST)
-#du = dt.strftime('%Y-%m-%d')
-#ap="https://api.nhk.or.jp/v2/pg/list/440/tv/"+du+".json?key=t4fDaYNIS6qmUqAhy3ihGdWTh8Lf1N5M"
+t_delta = datetime.timedelta(hours=9)  
+JST = datetime.timezone(t_delta, 'JST')  
+dt = datetime.datetime.now(JST)
+du = dt.strftime('%Y-%m-%d')
+ap="https://api.nhk.or.jp/v2/pg/list/440/tv/"+du+".json?key=t4fDaYNIS6qmUqAhy3ihGdWTh8Lf1N5M"
 
-#url = requests.get(ap)
-#text = url.text
-#data = json.loads(text)["list"]
-#tv = data["g1"]+data["g2"]+data["e1"]+data["s1"]+data["s3"]
+url = requests.get(ap)
+text = url.text
+data = json.loads(text)["list"]
+tv = data["g1"]+data["g2"]+data["e1"]+data["s1"]+data["s3"]
 
-#df = pd.DataFrame((list(tv)), columns = ['title','start_time','subtitle','act','content'])
-#az=(df.query('title.str.contains(@z)', engine='python'))
-#print(az)
-#st.DataFrame(az)
+df = pd.DataFrame((list(tv)), columns = ['title','start_time','subtitle','act','content'])
+az=(df.query('title.str.contains(@z)', engine='python'))
+print(az)
+st.DataFrame(az)
