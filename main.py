442d2879ff9237b306a2476ce3d7bbc81c052f84
YAMA-Te <99267757+YAMA-Te@users.noreply.github.com>
2022-02-10 23:50:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ tv = data["g1"]+data["g2"]+data["e1"]+data["s1"]+data["s3"]
 
 df = pd.DataFrame((list(tv)), columns = ['title','start_time','subtitle','act','content'])
 az=(df.query('title.str.contains(@z)', engine='python'))
-st.dataframe(az.set_option("display.max_colwidth", 200))
+st.dataframe(az)
