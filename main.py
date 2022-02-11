163d5c1d084027be9ccc37d934847f1c941cf666
YAMA-Te <99267757+YAMA-Te@users.noreply.github.com>
2022-02-11 17:20:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ import re
 
 st.title("大分県関係の番組")
 
-z=("大分")
+now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+for i in range(0,8):
+    dayy = now+datetime.timedelta(days=i)
+    du = dayy.strftime('%Y-%m-%d')
+    ap="https://api.nhk.or.jp/v2/pg/list/440/tv/"+du+".json?key=t4fDaYNIS6qmUqAhy3ihGdWTh8Lf1N5M"
+    url = requests.get(ap)
+    text = url.text
+    data = json.loads(text)["list"]
+    tv = data["g1"]+data["e1"]+data["s1"]+data["s3"]
 
-t_delta = datetime.timedelta(hours=9)  
-JST = datetime.timezone(t_delta, 'JST')  
-dt = datetime.datetime.now(JST)
-du = dt.strftime('%Y-%m-%d')
-ap="https://api.nhk.or.jp/v2/pg/list/440/tv/"+du+".json?key=t4fDaYNIS6qmUqAhy3ihGdWTh8Lf1N5M"
-
-url = requests.get(ap)
-text = url.text
-data = json.loads(text)["list"]
-tv = data["g1"]+data["g2"]+data["e1"]+data["s1"]+data["s3"]
-
-
-df = pd.DataFrame((list(tv)), columns = ['title','start_time','subtitle','act','content'])
-az=(df.query('title.str.contains(@z)', engine='python'))
+    df = pd.DataFrame((list(tv)), columns = ['start_time','title','subtitle','act','content'])
+    z=("大分")
+    az=(df.query('title.str.contains(@z)|\
+                 subtitle.str.contains(@z)|\
+                 content.str.contains(@z)|\
+                 act.str.contains(@z)', engine='python'))
 st.dataframe(az)
