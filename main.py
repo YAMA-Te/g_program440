454c878a807523dae37a36b62b4dd8ccacb4965c
YAMA-Te <99267757+YAMA-Te@users.noreply.github.com>
2022-02-11 22:39:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,25 @@ import re
 import pytz
 
 st.title("大分県関係の番組")
-
 now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+lst=[]
 for i in range(0,8):
     dayy = now+datetime.timedelta(days=i)
     du = dayy.strftime('%Y-%m-%d')
-    ap="https://api.nhk.or.jp/v2/pg/list/440/tv/"+du+".json?key=t4fDaYNIS6qmUqAhy3ihGdWTh8Lf1N5M"
+    ap="https://api.nhk.or.jp/v2/pg/list/440/tv/"+du+".json?key=xQUIY1xXNVtrG9AQ1OWCdfHh4kD9iW1e"
     url = requests.get(ap)
     text = url.text
     data = json.loads(text)["list"]
-    tv = data["g1"]+data["e1"]+data["s1"]+data["s3"]
+    lst.append(text)
 
-    df = pd.DataFrame((list(tv)), columns = ['start_time','title','subtitle','act','content'])
-    z=("大分")
-    az=(df.query('title.str.contains(@z)|\
-                 subtitle.str.contains(@z)|\
-                 content.str.contains(@z)|\
-                 act.str.contains(@z)', engine='python')
-az1=df.join(az)
-st.dataframe(az1)
+df1 = pd.DataFrame(lst) 
+#print(df1)
+#print(type(lst))
+tv = df1["g1"]+df1["e1"]+df1["s1"]+df1["s3"]
+df = pd.DataFrame((list(df1)), columns = ['start_time','title','subtitle','act','content'])
+z=("大分")
+az=(df.query('title.str.contains(@z)|\
+              subtitle.str.contains(@z)|\
+              content.str.contains(@z)|\
+              act.str.contains(@z)', engine='python'))
+st.dataframe(az)
